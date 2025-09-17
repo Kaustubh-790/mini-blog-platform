2182878edbe666132847e677953b667d80d02d7b
Kaustubh-790 <kaustubhsharma434@gmail.com>
2025-09-17 15:15:57 +0530

    design changes

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -12,7 +12,7 @@ export function Layout() {
   }
 
   return (
-    <div className="min-h-screen flex flex-col bg-background">
+    <div className="min-h-screen flex flex-col bg-transparent">
       <Navbar />
       <main className="flex-1">
         <Outlet />
